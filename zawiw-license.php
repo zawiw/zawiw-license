c1a7a451ee4627250d8121768d0436e55c1097d3
georg-e <georg.eisenhart@uni-ulm.de>
2015-09-10 15:25:17 +0200

    initial commit

---
+++ zawiw-license.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Zawiw License
+Plgin URI:
+Description: Wordpress Plugin for media licenses
+Version: 0.1
+Author: Georg Eisenhart, Valentin Knabel
+Author URI:
+License: MIT
+*/
+
+?>
